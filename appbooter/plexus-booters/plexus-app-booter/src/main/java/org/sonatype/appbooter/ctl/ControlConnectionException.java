2e17e4efc80c9f055c87b9cf0383b5c4556281a9
brianf <brianf@5751e0cb-dcb7-432f-92e2-260806df54be>
2008-04-30 01:20:42 +0000

    update header
    git-svn-id: https://svn.sonatype.org/spice/trunk@10 5751e0cb-dcb7-432f-92e2-260806df54be

--- appbooter/plexus-booters/plexus-app-booter/src/main/java/org/sonatype/appbooter/ctl/ControlConnectionException.java
+++ appbooter/plexus-booters/plexus-app-booter/src/main/java/org/sonatype/appbooter/ctl/ControlConnectionException.java
@@ -1,5 +1,5 @@
  /**
-  * Copyright (C) 2008 Sonatype, Inc. 
+  * Copyright (C) 2008 Sonatype Inc. 
   * Sonatype Inc, licenses this file to you under the Apache License,
   * Version 2.0 (the "License"); you may not use this file except in 
   * compliance with the License.  You may obtain a copy of the License at
