f9cae5d6be17b4c3038c71a724ad6b5cb5e81f00
jvanzyl <jvanzyl@5751e0cb-dcb7-432f-92e2-260806df54be>
2008-10-26 21:53:33 +0000

    o full example of creating a plexus runtime using JSW
    git-svn-id: https://svn.sonatype.org/spice/trunk@423 5751e0cb-dcb7-432f-92e2-260806df54be

---
+++ sandbox/plexus-distro/src/main/java/org/sonatype/plexus/distro/App.java
@@ -0,0 +1,13 @@
+package org.sonatype.plexus.distro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
