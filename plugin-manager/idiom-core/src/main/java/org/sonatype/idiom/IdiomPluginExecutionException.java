d3d690a058c9a9ad1f03faed401c5ab032837574
jvanzyl <jvanzyl@5751e0cb-dcb7-432f-92e2-260806df54be>
2009-01-20 19:23:28 +0000

    git-svn-id: https://svn.sonatype.org/spice/trunk@723 5751e0cb-dcb7-432f-92e2-260806df54be

---
+++ plugin-manager/idiom-core/src/main/java/org/sonatype/idiom/IdiomPluginExecutionException.java
@@ -0,0 +1,23 @@
+package org.sonatype.idiom;
+
+public class IdiomPluginExecutionException
+    extends Exception
+{
+    public IdiomPluginExecutionException( String arg0, Throwable arg1 )
+    {
+        super( arg0, arg1 );
+        // TODO Auto-generated constructor stub
+    }
+
+    public IdiomPluginExecutionException( String arg0 )
+    {
+        super( arg0 );
+        // TODO Auto-generated constructor stub
+    }
+
+    public IdiomPluginExecutionException( Throwable arg0 )
+    {
+        super( arg0 );
+        // TODO Auto-generated constructor stub
+    }
+}
