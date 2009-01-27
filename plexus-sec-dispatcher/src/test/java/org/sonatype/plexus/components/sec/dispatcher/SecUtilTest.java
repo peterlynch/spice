70d56456ffca0e85388cede7a3af4ec0c74e9bfb
oleg <oleg@5751e0cb-dcb7-432f-92e2-260806df54be>
2009-01-27 01:16:24 +0000

    adjusted license
    git-svn-id: https://svn.sonatype.org/spice/trunk@775 5751e0cb-dcb7-432f-92e2-260806df54be

--- plexus-sec-dispatcher/src/test/java/org/sonatype/plexus/components/sec/dispatcher/SecUtilTest.java
+++ plexus-sec-dispatcher/src/test/java/org/sonatype/plexus/components/sec/dispatcher/SecUtilTest.java
@@ -1,37 +1,30 @@
-/*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
+/**
+ * Copyright (c) 2008 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+ 
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-*/
 
 package org.sonatype.plexus.components.sec.dispatcher;
 
 import java.io.FileWriter;
 import java.util.Map;
 
+import junit.framework.TestCase;
+
+import org.sonatype.plexus.components.cipher.DefaultPlexusCipher;
 import org.sonatype.plexus.components.sec.dispatcher.model.Config;
 import org.sonatype.plexus.components.sec.dispatcher.model.ConfigProperty;
 import org.sonatype.plexus.components.sec.dispatcher.model.Sec;
 import org.sonatype.plexus.components.sec.dispatcher.model.io.xpp3.SecurityConfigurationXpp3Writer;
-import org.sonatype.plexus.components.cipher.DefaultPlexusCipher;
-import org.sonatype.plexus.components.cipher.PBECipher;
-import org.sonatype.plexus.components.sec.dispatcher.DefaultSecDispatcher;
-import org.sonatype.plexus.components.sec.dispatcher.SecUtil;
-
-import junit.framework.TestCase;
 
 /**
  *
