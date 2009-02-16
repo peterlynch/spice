9f19432d06ee79d76519493dedfab7722210cef6
jvanzyl <jvanzyl@5751e0cb-dcb7-432f-92e2-260806df54be>
2009-02-16 21:28:46 +0000

    o share the same configuration between the test and the plx:run cases
    git-svn-id: https://svn.sonatype.org/spice/trunk@915 5751e0cb-dcb7-432f-92e2-260806df54be

--- plexus-webdav/src/test/java/org/sonatype/webdav/WebdavTest.java
+++ plexus-webdav/src/test/java/org/sonatype/webdav/WebdavTest.java
@@ -1,14 +1,30 @@
 package org.sonatype.webdav;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.codehaus.plexus.ContainerConfiguration;
 import org.codehaus.plexus.PlexusTestCase;
 import org.sonatype.plexus.webcontainer.ServletContainer;
 
-public class WebdavTest
-	extends PlexusTestCase
+public class WebdavTest extends PlexusTestCase 
 {
-    public void testWebdav()
-        throws Exception
+	public void testWebdav() throws Exception 
 	{
 		lookup( ServletContainer.class );
 	}
+
+    protected void customizeContainerConfiguration( ContainerConfiguration containerConfiguration )
+    {
+    	try 
+    	{
+    		URL url = new File( getBasedir(), "src/main/plexus/plexus.xml" ).toURI().toURL();
+			containerConfiguration.setContainerConfigurationURL( url );
+		} 
+    	catch (MalformedURLException e) 
+    	{
+			// do nothing
+		}
+    }
 }
