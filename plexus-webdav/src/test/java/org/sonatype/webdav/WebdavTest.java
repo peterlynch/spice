541aa4164bf68b855f4a2639079735cb52027e97
oleg <oleg@5751e0cb-dcb7-432f-92e2-260806df54be>
2009-03-03 22:26:09 +0000

    added a test for reading file from the server
    git-svn-id: https://svn.sonatype.org/spice/trunk@1003 5751e0cb-dcb7-432f-92e2-260806df54be

--- plexus-webdav/src/test/java/org/sonatype/webdav/WebdavTest.java
+++ plexus-webdav/src/test/java/org/sonatype/webdav/WebdavTest.java
@@ -1,6 +1,8 @@
 package org.sonatype.webdav;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -10,9 +12,46 @@ import org.sonatype.plexus.webcontainer.ServletContainer;
 
 public class WebdavTest extends PlexusTestCase 
 {
+
+    File _testFile;
+    
+    String _testText;
+    
+    @Override
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+        
+        _testFile = new File( "./target/webdav/test.txt" );
+        _testFile.deleteOnExit();
+        
+        _testFile.getParentFile().mkdirs();
+        
+        _testText = System.currentTimeMillis() +" ms";
+        
+        FileOutputStream out = new FileOutputStream( _testFile );
+        out.write( _testText.getBytes() );
+        out.flush();
+        out.close();
+    }
+    
 	public void testWebdav() throws Exception 
 	{
 		lookup( ServletContainer.class );
+		
+		URL url = new URL("http://localhost:9000/webdav");
+		
+		InputStream in = url.openStream();
+		
+		byte [] buf = new byte [ 32 ];
+		
+		int n = in.read( buf );
+		
+		in.close();
+		
+		assertEquals( _testText.getBytes().length << 1 , n );
+		
 	}
 
     protected void customizeContainerConfiguration( ContainerConfiguration containerConfiguration )
