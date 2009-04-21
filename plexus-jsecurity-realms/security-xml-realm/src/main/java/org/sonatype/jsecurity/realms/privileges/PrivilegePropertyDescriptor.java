e64d3bd27a33f125b22be09bb3fbbcf09a57d352
tstevens <tstevens@5751e0cb-dcb7-432f-92e2-260806df54be>
2009-04-21 15:04:02 +0000

    NEXUS-1787, NEXUS-1773, Moved security out of nexus, moved realms into this tree
    git-svn-id: https://svn.sonatype.org/spice/trunk@1186 5751e0cb-dcb7-432f-92e2-260806df54be

--- plexus-jsecurity-realms/security-xml-realm/src/main/java/org/sonatype/jsecurity/realms/privileges/PrivilegePropertyDescriptor.java
+++
@@ -1,9 +0,0 @@
-package org.sonatype.jsecurity.realms.privileges;
-
-public interface PrivilegePropertyDescriptor
-{
-    String getId();
-    String getHelpText();
-    String getName();
-    String getType();
-}
