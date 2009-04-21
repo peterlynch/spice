870caef516a8a6a675320c83715adbef0029c81e
juven <juven@5751e0cb-dcb7-432f-92e2-260806df54be>
2009-04-21 07:25:33 +0000

    [NEXUS-1930] unit test the timeline search functionality
    git-svn-id: https://svn.sonatype.org/spice/trunk@1180 5751e0cb-dcb7-432f-92e2-260806df54be

--- spice-timeline/src/test/java/org/sonatype/timeline/TimelineIndexerTest.java
+++ spice-timeline/src/test/java/org/sonatype/timeline/TimelineIndexerTest.java
@@ -45,7 +45,6 @@ public class TimelineIndexerTest
             {
                 file.delete();
             }
-
             indexDirectory.delete();
         }
 
@@ -55,19 +54,7 @@ public class TimelineIndexerTest
     public void testIndexOneRecord()
         throws Exception
     {
-        long timestamp = new Date().getTime();
-        String type = "type";
-        String subType = "subType";
-        Map<String, String> data = new HashMap<String, String>();
-        data.put( "k1 test", "v1" );
-        data.put( "k2", "v2" );
-
-        TimelineRecord record = new TimelineRecord();
-        record.setTimestamp( timestamp );
-        record.setType( type );
-        record.setSubType( subType );
-        record.setData( data );
-
+        TimelineRecord record = createTimelineRecord();
         indexer.add( record );
 
         Set<String> types = new HashSet<String>();
@@ -75,6 +62,266 @@ public class TimelineIndexerTest
         List<Map<String, String>> results = indexer.retrieve( 0, System.currentTimeMillis(), types, null, 0, 100, null );
 
         assertEquals( 1, results.size() );
-        assertEquals( data, results.get( 0 ) );
+        assertEquals( record.getData(), results.get( 0 ) );
+    }
+
+    public void testIndexMutipleRecords()
+        throws Exception
+    {
+        for ( int i = 0; i < 10; i++ )
+        {
+            indexer.add( createTimelineRecord() );
+        }
+
+        List<Map<String, String>> results = indexer.retrieve( 0, System.currentTimeMillis(), null, null, 0, 100, null );
+
+        assertEquals( 10, results.size() );
+    }
+
+    public void testSearchByTime()
+        throws Exception
+    {
+        TimelineRecord rec1 = createTimelineRecord();
+        rec1.setTimestamp( 1000000L );
+        TimelineRecord rec2 = createTimelineRecord();
+        rec2.setTimestamp( 2000000L );
+        TimelineRecord rec3 = createTimelineRecord();
+        rec3.setTimestamp( 3000000L );
+        TimelineRecord rec4 = createTimelineRecord();
+        rec4.setTimestamp( 4000000L );
+        indexer.add( rec1 );
+        indexer.add( rec2 );
+        indexer.add( rec3 );
+        indexer.add( rec4 );
+
+        assertEquals( 4, indexer.retrieve( 0, System.currentTimeMillis(), null, null, 0, 100, null ).size() );
+        assertEquals( 3, indexer.retrieve( 0, 3500000L, null, null, 0, 100, null ).size() );
+        assertEquals( 2, indexer.retrieve( 1500000L, 3500000L, null, null, 0, 100, null ).size() );
+        assertEquals( 0, indexer.retrieve( 4500000L, System.currentTimeMillis(), null, null, 0, 100, null ).size() );
+    }
+
+    public void testSearchByType()
+        throws Exception
+    {
+        TimelineRecord rec1 = createTimelineRecord();
+        rec1.setType( "typeA" );
+        TimelineRecord rec2 = createTimelineRecord();
+        rec2.setType( "typeB" );
+        TimelineRecord rec3 = createTimelineRecord();
+        rec3.setType( "typeB" );
+        TimelineRecord rec4 = createTimelineRecord();
+        rec4.setType( "typeC" );
+        indexer.add( rec1 );
+        indexer.add( rec2 );
+        indexer.add( rec3 );
+        indexer.add( rec4 );
+
+        Set<String> types = new HashSet<String>();
+
+        types.add( "typeA" );
+        assertEquals( 1, indexer.retrieve( 0, System.currentTimeMillis(), types, null, 0, 100, null ).size() );
+        types.clear();
+        types.add( "typeB" );
+        assertEquals( 2, indexer.retrieve( 0, System.currentTimeMillis(), types, null, 0, 100, null ).size() );
+        types.clear();
+        types.add( "typeA" );
+        types.add( "typeB" );
+        assertEquals( 3, indexer.retrieve( 0, System.currentTimeMillis(), types, null, 0, 100, null ).size() );
+        types.clear();
+        types.add( "typeA" );
+        types.add( "typeB" );
+        types.add( "typeC" );
+        assertEquals( 4, indexer.retrieve( 0, System.currentTimeMillis(), types, null, 0, 100, null ).size() );
+        types.clear();
+        assertEquals( 4, indexer.retrieve( 0, System.currentTimeMillis(), types, null, 0, 100, null ).size() );
+        types.clear();
+        types.add( "typeX" );
+        assertEquals( 0, indexer.retrieve( 0, System.currentTimeMillis(), types, null, 0, 100, null ).size() );
+    }
+
+    public void testSearchBySubType()
+        throws Exception
+    {
+        TimelineRecord rec1 = createTimelineRecord();
+        rec1.setSubType( "subA" );
+        TimelineRecord rec2 = createTimelineRecord();
+        rec2.setSubType( "subB" );
+        TimelineRecord rec3 = createTimelineRecord();
+        rec3.setSubType( "subB" );
+        TimelineRecord rec4 = createTimelineRecord();
+        rec4.setSubType( "subC" );
+        indexer.add( rec1 );
+        indexer.add( rec2 );
+        indexer.add( rec3 );
+        indexer.add( rec4 );
+
+        Set<String> subTypes = new HashSet<String>();
+
+        subTypes.add( "subA" );
+        assertEquals( 1, indexer.retrieve( 0, System.currentTimeMillis(), null, subTypes, 0, 100, null ).size() );
+        subTypes.clear();
+        subTypes.add( "subB" );
+        assertEquals( 2, indexer.retrieve( 0, System.currentTimeMillis(), null, subTypes, 0, 100, null ).size() );
+        subTypes.clear();
+        subTypes.add( "subA" );
+        subTypes.add( "subB" );
+        assertEquals( 3, indexer.retrieve( 0, System.currentTimeMillis(), null, subTypes, 0, 100, null ).size() );
+        subTypes.clear();
+        subTypes.add( "subA" );
+        subTypes.add( "subB" );
+        subTypes.add( "subC" );
+        assertEquals( 4, indexer.retrieve( 0, System.currentTimeMillis(), null, subTypes, 0, 100, null ).size() );
+        subTypes.clear();
+        assertEquals( 4, indexer.retrieve( 0, System.currentTimeMillis(), null, subTypes, 0, 100, null ).size() );
+        subTypes.clear();
+        subTypes.add( "subX" );
+        assertEquals( 0, indexer.retrieve( 0, System.currentTimeMillis(), null, subTypes, 0, 100, null ).size() );
+    }
+
+    public void testSearchByTypeAndSubType()
+        throws Exception
+    {
+        TimelineRecord rec1 = createTimelineRecord();
+        rec1.setType( "typeA" );
+        rec1.setSubType( "subX" );
+        TimelineRecord rec2 = createTimelineRecord();
+        rec2.setType( "typeB" );
+        rec2.setSubType( "subX" );
+        TimelineRecord rec3 = createTimelineRecord();
+        rec3.setType( "typeB" );
+        rec3.setSubType( "subY" );
+        TimelineRecord rec4 = createTimelineRecord();
+        rec4.setType( "typeA" );
+        rec4.setSubType( "subX" );
+        indexer.add( rec1 );
+        indexer.add( rec2 );
+        indexer.add( rec3 );
+        indexer.add( rec4 );
+
+        Set<String> types = new HashSet<String>();
+        Set<String> subTypes = new HashSet<String>();
+
+        types.add( "typeA" );
+        subTypes.add( "subX" );
+        assertEquals( 2, indexer.retrieve( 0, System.currentTimeMillis(), types, subTypes, 0, 100, null ).size() );
+
+        types.clear();
+        subTypes.clear();
+        types.add( "typeB" );
+        subTypes.add( "subX" );
+        assertEquals( 1, indexer.retrieve( 0, System.currentTimeMillis(), types, subTypes, 0, 100, null ).size() );
+
+        types.clear();
+        subTypes.clear();
+        types.add( "typeA" );
+        types.add( "typeB" );
+        subTypes.add( "subX" );
+        assertEquals( 3, indexer.retrieve( 0, System.currentTimeMillis(), types, subTypes, 0, 100, null ).size() );
+
+        types.clear();
+        subTypes.clear();
+        types.add( "typeA" );
+        subTypes.add( "subY" );
+        assertEquals( 0, indexer.retrieve( 0, System.currentTimeMillis(), types, subTypes, 0, 100, null ).size() );
+
+        types.clear();
+        subTypes.clear();
+        types.add( "typeB" );
+        subTypes.add( "subY" );
+        assertEquals( 1, indexer.retrieve( 0, System.currentTimeMillis(), types, subTypes, 0, 100, null ).size() );
+
+        types.clear();
+        subTypes.clear();
+        types.add( "typeA" );
+        types.add( "typeB" );
+        subTypes.add( "subX" );
+        subTypes.add( "subY" );
+        assertEquals( 4, indexer.retrieve( 0, System.currentTimeMillis(), types, subTypes, 0, 100, null ).size() );
+    }
+
+    public void testSearchByCount()
+        throws Exception
+    {
+        for ( int i = 0; i < 50; i++ )
+        {
+            indexer.add( createTimelineRecord() );
+        }
+
+        int count = 50;
+        assertEquals( count, indexer.retrieve( 0, System.currentTimeMillis(), null, null, 0, count, null ).size() );
+        count = 49;
+        assertEquals( count, indexer.retrieve( 0, System.currentTimeMillis(), null, null, 0, count, null ).size() );
+        count = 25;
+        assertEquals( count, indexer.retrieve( 0, System.currentTimeMillis(), null, null, 0, count, null ).size() );
+        count = 0;
+        assertEquals( count, indexer.retrieve( 0, System.currentTimeMillis(), null, null, 0, count, null ).size() );
+        count = 1;
+        assertEquals( count, indexer.retrieve( 0, System.currentTimeMillis(), null, null, 0, count, null ).size() );
+    }
+
+    public void testSearchByFrom()
+        throws Exception
+    {
+        for ( int i = 0; i < 50; i++ )
+        {
+            indexer.add( createTimelineRecord() );
+        }
+
+        int from = 49;
+        assertEquals( 50 - from, indexer.retrieve( 0, System.currentTimeMillis(), null, null, from, 1000, null ).size() );
+        from = 1;
+        assertEquals( 50 - from, indexer.retrieve( 0, System.currentTimeMillis(), null, null, from, 1000, null ).size() );
+        from = 25;
+        assertEquals( 50 - from, indexer.retrieve( 0, System.currentTimeMillis(), null, null, from, 1000, null ).size() );
+        from = 0;
+        assertEquals( 50 - from, indexer.retrieve( 0, System.currentTimeMillis(), null, null, from, 1000, null ).size() );
+        from = 50;
+        assertEquals( 50 - from, indexer.retrieve( 0, System.currentTimeMillis(), null, null, from, 1000, null ).size() );
+    }
+
+    public void testSearchResultOrderByTime()
+        throws Exception
+    {
+        TimelineRecord rec1 = createTimelineRecord();
+        rec1.setTimestamp( 1000000L );
+        rec1.getData().put( "t", "1" );
+        TimelineRecord rec2 = createTimelineRecord();
+        rec2.setTimestamp( 2000000L );
+        rec2.getData().put( "t", "2" );
+        TimelineRecord rec3 = createTimelineRecord();
+        rec3.setTimestamp( 3000000L );
+        rec3.getData().put( "t", "3" );
+        TimelineRecord rec4 = createTimelineRecord();
+        rec4.setTimestamp( 4000000L );
+        rec4.getData().put( "t", "4" );        
+
+        indexer.add( rec2 );
+        indexer.add( rec1 );
+        indexer.add( rec4 );
+        indexer.add( rec3 );
+
+        List<Map<String, String>> results = indexer.retrieve( 0, System.currentTimeMillis(), null, null, 0, 100, null );
+
+        assertEquals( 4, results.size() );
+        assertEquals( "4", results.get( 0 ).get( "t" ) );
+        assertEquals( "3", results.get( 1 ).get( "t" ) );
+        assertEquals( "2", results.get( 2 ).get( "t" ) );
+        assertEquals( "1", results.get( 3 ).get( "t" ) );
+    }
+
+    private TimelineRecord createTimelineRecord()
+    {
+        TimelineRecord record = new TimelineRecord();
+        record.setTimestamp( System.currentTimeMillis() );
+        record.setType( "type" );
+        record.setSubType( "subType" );
+        Map<String, String> data = new HashMap<String, String>();
+        data.put( "k1", "v1" );
+        data.put( "k2", "v2" );
+        data.put( "k3", "v3" );
+        data.put( "k4", "v4" );
+        record.setData( data );
+
+        return record;
     }
 }
