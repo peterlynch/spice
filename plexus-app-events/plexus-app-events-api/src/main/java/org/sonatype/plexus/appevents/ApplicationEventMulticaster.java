0a9fdf67a9341428d8c49f20fda442865da577e1
cstamas <cstamas@5751e0cb-dcb7-432f-92e2-260806df54be>
2009-05-06 12:56:48 +0000

    Added ignores
    git-svn-id: https://svn.sonatype.org/spice/trunk@1287 5751e0cb-dcb7-432f-92e2-260806df54be

---
+++ plexus-app-events/plexus-app-events-api/src/main/java/org/sonatype/plexus/appevents/ApplicationEventMulticaster.java
@@ -0,0 +1,12 @@
+package org.sonatype.plexus.appevents;
+
+/**
+ * Central component serving as event hub for application.
+ * 
+ * @author cstamas
+ */
+public interface ApplicationEventMulticaster
+    extends EventMulticaster
+{
+
+}
