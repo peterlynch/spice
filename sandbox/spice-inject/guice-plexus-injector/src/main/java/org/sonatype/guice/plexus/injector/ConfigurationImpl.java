9fd3d06db78913154f8c46357c382a8022cae547
mcculls <mcculls@5751e0cb-dcb7-432f-92e2-260806df54be>
2009-09-28 11:45:39 +0000

    NEXUS-2152: provide annotation implementations to support runtime additions
    git-svn-id: https://svn.sonatype.org/spice/trunk@1558 5751e0cb-dcb7-432f-92e2-260806df54be

---
+++ sandbox/spice-inject/guice-plexus-injector/src/main/java/org/sonatype/guice/plexus/injector/ConfigurationImpl.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2009 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.guice.plexus.injector;
+
+import java.lang.annotation.Annotation;
+
+import org.codehaus.plexus.component.annotations.Configuration;
+
+public final class ConfigurationImpl
+    implements Configuration
+{
+    private final String name;
+
+    private final String value;
+
+    public ConfigurationImpl( final String name, final String value )
+    {
+        if ( null == name || null == value )
+        {
+            throw new IllegalArgumentException( "@" + Configuration.class.getName() + " cannot contain null values" );
+        }
+
+        this.name = name;
+        this.value = value;
+    }
+
+    public String name()
+    {
+        return name;
+    }
+
+    public String value()
+    {
+        return value;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return ( 127 * "name".hashCode() ^ name.hashCode() ) + ( 127 * "value".hashCode() ^ value.hashCode() );
+    }
+
+    @Override
+    public boolean equals( final Object rhs )
+    {
+        if ( rhs instanceof Configuration )
+        {
+            final Configuration conf = (Configuration) rhs;
+
+            return name.equals( conf.name() ) && value.equals( conf.value() );
+        }
+        return false;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "@" + Configuration.class.getName() + "(name=" + name + ", value=" + value + ")";
+    }
+
+    public Class<? extends Annotation> annotationType()
+    {
+        return Configuration.class;
+    }
+}
---
+++ sandbox/spice-inject/guice-plexus-injector/src/main/java/org/sonatype/guice/plexus/injector/RequirementImpl.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2009 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.guice.plexus.injector;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+
+import org.codehaus.plexus.component.annotations.Requirement;
+
+public final class RequirementImpl
+    implements Requirement
+{
+    private final Class<?> role;
+
+    private final String hint;
+
+    private final String[] hints;
+
+    public RequirementImpl( final Class<?> role, final String... hints )
+    {
+        if ( null == role || null == hints || Arrays.asList( hints ).contains( null ) )
+        {
+            throw new IllegalArgumentException( "@" + Requirement.class.getName() + " cannot contain null values" );
+        }
+
+        this.role = role;
+
+        if ( hints.length == 1 )
+        {
+            hint = hints[0];
+            this.hints = new String[0];
+        }
+        else
+        {
+            hint = "";
+            this.hints = hints;
+        }
+    }
+
+    public Class<?> role()
+    {
+        return role;
+    }
+
+    public String hint()
+    {
+        return hint;
+    }
+
+    public String[] hints()
+    {
+        return hints;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return ( 127 * "role".hashCode() ^ role.hashCode() ) + ( 127 * "hint".hashCode() ^ hint.hashCode() )
+            + ( 127 * "hints".hashCode() ^ Arrays.hashCode( hints ) );
+    }
+
+    @Override
+    public boolean equals( final Object rhs )
+    {
+        if ( rhs instanceof Requirement )
+        {
+            final Requirement req = (Requirement) rhs;
+
+            return role.equals( req.role() ) && hint.equals( req.hint() ) && Arrays.equals( hints, req.hints() );
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "@" + Requirement.class.getName() + "(role=" + role + ", hint=" + hint + ", hints="
+            + Arrays.toString( hints ) + ")";
+    }
+
+    public Class<? extends Annotation> annotationType()
+    {
+        return Requirement.class;
+    }
+}
