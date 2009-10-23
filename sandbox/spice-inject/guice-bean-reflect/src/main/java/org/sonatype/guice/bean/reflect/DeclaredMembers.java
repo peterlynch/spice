44d0db1e30f02a842eddf8a68edae3e7906405c6
mcculls <mcculls@5751e0cb-dcb7-432f-92e2-260806df54be>
2009-10-23 11:22:40 +0000

    NEXUS-2150: change name because this reflection is not plexus specific
    git-svn-id: https://svn.sonatype.org/spice/trunk@1651 5751e0cb-dcb7-432f-92e2-260806df54be

--- sandbox/spice-inject/guice-bean-reflect/src/main/java/org/sonatype/guice/bean/reflect/DeclaredMembers.java
+++ sandbox/spice-inject/guice-bean-reflect/src/main/java/org/sonatype/guice/bean/reflect/DeclaredMembers.java
@@ -10,7 +10,7 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-package org.sonatype.guice.plexus.reflect;
+package org.sonatype.guice.bean.reflect;
 
 import java.lang.reflect.Member;
 import java.util.Iterator;
