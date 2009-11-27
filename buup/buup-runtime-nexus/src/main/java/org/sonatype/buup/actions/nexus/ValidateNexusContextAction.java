d0d8b058030822ccfeeabd8755ec53ce6d20c178
cstamas <cstamas@5751e0cb-dcb7-432f-92e2-260806df54be>
2009-11-27 14:34:23 +0000

    Added nexus specific runtime
    git-svn-id: https://svn.sonatype.org/spice/trunk@1790 5751e0cb-dcb7-432f-92e2-260806df54be

---
+++ buup/buup-runtime-nexus/src/main/java/org/sonatype/buup/actions/nexus/ValidateNexusContextAction.java
@@ -0,0 +1,16 @@
+package org.sonatype.buup.actions.nexus;
+
+import org.sonatype.buup.actions.AbstractAction;
+
+public class ValidateNexusContextAction
+    extends AbstractAction
+{
+
+    public void perform()
+        throws Exception
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
